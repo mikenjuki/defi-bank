19b510e9d7d0818b6b67055ba2c6be1820a94bed
Michael Njuki <mikenjuki@gmail.com>
2023-03-28 14:47:06 +0300

    created and styled the GetStarted component

--- src/components/GetStarted.jsx
+++ src/components/GetStarted.jsx
@@ -1,7 +1,30 @@
 import React from "react";
+import styles from "../styles";
+import { arrowUp } from "../assets";
 
-const GetStarted = () => {
-  return <div>GetStarted</div>;
-};
+const GetStarted = () => (
+  <div
+    className={`${styles.flexCenter} w-[140px] h-[140px] rounded-full bg-blue-gradient cursor-pointer p-[2px]`}
+  >
+    <div
+      className={`${styles.flexCenter} flex flex-col bg-primary w-full h-full rounded-full`}
+    >
+      <div className={`${styles.flexStart} flex-row`}>
+        <p className="font-main font-medium text-[18px] leading-[23px]">
+          <span className="text-gradient">Get</span>
+        </p>
+        <img
+          src={arrowUp}
+          className="object-contain h-[23px] w-[23px]"
+          alt="arrow icon"
+        />
+      </div>
+
+      <p className="font-main font-medium text-[18px] leading-[23px]">
+        <span className="text-gradient">Started</span>
+      </p>
+    </div>
+  </div>
+);
 
 export default GetStarted;
