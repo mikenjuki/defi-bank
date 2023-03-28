391d8f875acd7ca82f9e73200e7b29a6f7149b5a
Michael Njuki <mikenjuki@gmail.com>
2023-03-28 10:52:40 +0300

    Setup project and folder structure

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <div className="bg-primary w-full h-32 overflow-hidden">
+        <h1 className="text-white">Hi Mike</h1>
+      </div>
+    </div>
+  );
+};
+
+export default App;
