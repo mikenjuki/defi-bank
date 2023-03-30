76e9ea6fb78b81047ad866600835172e44e41305
Michael Njuki <mikenjuki@gmail.com>
2023-03-30 13:21:56 +0300

    Added some hover effects to the client component

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import { clients } from "../constants";
+import styles from "../styles";
 
-const Clients = () => {
-  return <div>Clients</div>;
-};
+const Clients = () => (
+  <section className={`${styles.flexCenter} my-4`}>
+    <div className={`${styles.flexCenter} flex-wrap w-full`}>
+      {clients.map((client) => (
+        <div
+          key={client.id}
+          className={`${styles.flexCenter} flex-1 min-w-[120px] sm:min-w-[192px] cursor-pointer`}
+        >
+          <img
+            src={client.logo}
+            alt={client.id}
+            className="w-[100px] sm:w-[192px] object-contain filter brightness-75 grayscale-100 hover:brightness-200 hover:grayscale-0"
+          />
+        </div>
+      ))}
+    </div>
+  </section>
+);
 
 export default Clients;
