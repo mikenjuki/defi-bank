65adf9f1d3b9020e709d940c83c9fe3c9a8d9d16
Michael Njuki <mikenjuki@gmail.com>
2023-03-30 10:35:41 +0300

    Completed the CardDeal Component

--- src/components/CardDeal.jsx
+++ src/components/CardDeal.jsx
@@ -1,7 +1,30 @@
 import React from "react";
+import styles, { layout } from "../styles";
+import { card } from "../assets";
+import Button from "./Button";
 
-const CardDeal = () => {
-  return <div>CardDeal</div>;
-};
+const CardDeal = () => (
+  <section className={`${layout.section}`}>
+    <div className={`${layout.sectionInfo}`}>
+      <h2 className={`${styles.heading2}`}>
+        Find a better card deal
+        <br className="hidden sm:block" />
+        in few easy steps.
+      </h2>
+
+      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+        Looking for a better card deal? Look no further than Hoobank! Our
+        easy-to-use system makes it simple to find the best deals for you. With
+        just a few easy steps, you can find the perfect card deal that fits your
+        needs.
+      </p>
+
+      <Button styles="mt-10" />
+    </div>
+    <div className={`${layout.sectionImg}`}>
+      <img src={card} alt="card" className="h-full w-full" />
+    </div>
+  </section>
+);
 
 export default CardDeal;
